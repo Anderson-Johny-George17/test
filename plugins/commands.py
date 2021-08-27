295afdc9c3b485e07b4ec17f9bb1465cf677d27e
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-08-27 15:03:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,7 +27,8 @@ async def start(bot, cmd):
                 ident, file_id = cmd.text.split("_-_-_-_")
                 await bot.send_message(
                     chat_id=cmd.from_user.id,
-                    text="**Please Join My Updates Channel to use this Bot!**",
+                    text="""**🔊 Please Join Our Main Channel For Files <a href="https://t.me/cinemazilla2">CʜᴀɴɴᴇL</a>🔊
+താഴെ കാണുന്ന ചാനലിൽ ജോയിൻ ചെയ്തതിനു ശേഷം 'try again' ക്ലിക്ക് ചെയ്താൽ നിങ്ങളുടെ ഫയൽ ലഭിക്കുന്നതാണ്!!"""**,
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
